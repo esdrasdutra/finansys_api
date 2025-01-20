711ffafdd8c06478803d7edfb7134c7d9fb6a2e9
esdrasdutra <esdrasfdutra@gmail.com>
2025-01-20 13:01:53 -0300

    UpdateDB Config

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,8 @@
 import pathlib
 
-from pydantic import AnyHttpUrl, EmailStr, validator, BaseSettings
+from pydantic import AnyHttpUrl, EmailStr, validator
+
+from pydantic_settings import BaseSettings
 
 from typing import List, Optional, Union
 
